9a0ab22b4b9769c8198d1b8d2d99de7c567b0d56
Ramirez <jramirez1@FVFZMXVMJ1WK.tld>
2022-03-25 10:27:52 -0500

    Not finished, need to ask help from Ate

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 function askName(){
     var n = prompt("What is your name?")
-
     return n
-}
+} // 10
 
 function welcome(a){
     alert(`Welcome back, ${a}, to the newly improved temperature converter with functions!`)
@@ -14,17 +13,36 @@ function choice(){
     var a = prompt("Are you trying to convert Farenheight to Celcius (c) or are you trying to convert Celcius to Farenheight (f)?")
 
     return a
+} // 15
+
+function choiceAns(a){
+    if(a == "f"){
+        
+    } else if (a == "c"){
+
+    }
 }
 
 function askTempCel(){
-    var c = prompt("What is the temp in Celcius?")
+    var c = Number(prompt("What is the temp in Celcius?"))
     return c
+} // 20
+
+function ctof(a){
+    var b = (((a * 9) / 5) + 32)
+    return b
+} // 20
+
+ctof(askTempCel())
+
+console.log(ctof())
+
+function farenAns(a, b){
+    alert(`Because it is ${a} degrees celcius, then it is ${b} degrees farenheight outside.`)
 }
 
-function choiceAns(b){
-    if (b == "f"){
+farenAns(askTempCel(), ctof())
 
-    } else if (b == "c"){
-        
-    }
-}
+console.log(farenAns())
+
+//80
